5d5fe70e3a60f51c2a17528d18597b5d7e9f15e8
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-22 22:57:15 +0900

    Pdf()

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const crawlSomething = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   try {
-    await page.setViewport({
-      width: 1920,
-      height: 1080,
+    await page.goto('https://news.ycombinator.com', {
+      waitUntil: 'networkidle2',
     });
-    await page.goto('https://example.com');
-    await page.screenshot({
-      path: 'example.png',
+    await page.pdf({
+      path: 'example.pdf',
+      format: 'a4',
     });
     await browser.close();
   } catch (e) {
