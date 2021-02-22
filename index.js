fe55c50b8506e6aa86ecf3fe090fdbd442ab2b7b
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-02-22 22:22:15 +0900

    setViewPort()

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const crawlSomething = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   try {
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+    });
     await page.goto('https://example.com');
     await page.screenshot({
       path: 'example.png',
